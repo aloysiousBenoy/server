7e8ba4c8329cfab2f37b5c3570c9fcace39dd79e
aloysiousbenoy <aloysiousbenoy@gmail.com>
2020-06-13 19:59:12 +0530

    Initial commit. http server setup succesfully

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+
+const server= http.createServer((req,res)=>{
+    console.log(req.headers);
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/html');
+    res.end("<html><body><h1>Hello, server here!</h1></body></html>");
+});
+
+const hostname="localhost";
+const port = 3000;
+
+server.listen(port,hostname,()=>{
+    console.log(`Server listening at http://${hostname}:${port}`);
+});
